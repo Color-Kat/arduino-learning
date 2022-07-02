200e71d9530065395331ce8ed7e988797650203e
Color-Kat <katik7979@gmail.com>
2022-07-02 12:30:59 +0300

    button without pullup, with 10k resistor

--- digitalPins/digitalPins.ino
+++ digitalPins/digitalPins.ino
@@ -9,16 +9,20 @@ boolean light = 0;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(speaker_pin, INPUT_PULLUP);
+    pinMode(button_pin, INPUT);
     pinMode(nano_lamp, OUTPUT);
 }
   
 void loop() {
-    boolean button = !digitalRead(button_pin);
+    boolean button = digitalRead(button_pin);
+    digitalWrite(nano_lamp, 1);
 
+    Serial.println(button);
 
-    if(button == 1) digitalWrite(nano_lamp, 1);
-    else digitalWrite(nano_lamp, 0);
+    if(button == 1) light = 1;
+    else light = 0;
+
+    
     
   
 //    if(millis() - last_time > 1000) {
