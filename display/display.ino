c2d12b1eec3e683582ce36090d24bd438b692579
Color-Kat <katik7979@gmail.com>
2022-07-09 18:30:31 +0300

    relay + display + DS18b20. Power by dc-dc

--- display/display.ino
+++ display/display.ino
@@ -1,14 +1,18 @@
 #include <microDS18B20.h>
 #include <GyverTimers.h>
-#include "GyverTM1637.h"
+#include <GyverTM1637.h>
 
 // Термометр
-MicroDS18B20<5> ds;
+MicroDS18B20<A2> ds;
 
 // Дисплей
-#define CLK 6
-#define DIO 7
+#define CLK 12
+#define DIO 11
 GyverTM1637 disp(CLK, DIO);
+
+// Реле
+#define relay_pin 8
+boolean isRelay = false;
 
 //uint32_t Now, clocktimer;
 //boolean flag;
@@ -18,12 +22,12 @@ int temperature;
 void setup(){
   Serial.begin(9600);
 
+  pinMode(relay_pin, OUTPUT);
+
   disp.clear();
   disp.brightness(7);  // яркость, 0 - 7 (минимум - максимум)
   disp.clear();
   disp.displayByte(_t, _e, _a, _empty);
-//  Timer2.setPeriod(3000);
-//  Timer2.enableISR();
 }
 
 void loop() {
@@ -31,17 +35,14 @@ void loop() {
   if(millis() - timer >= 1000){
       timer = millis();
 
+      isRelay = !isRelay;
+      digitalWrite(relay_pin, isRelay);
+
       if(ds.readTemp()){
           temperature = ds.getTemp();
           Serial.println(temperature);
-//          disp.clear();
           disp.displayInt(temperature);
       }
       ds.requestTemp();
   }
 }
-
-//ISR(TIMER2_A) {
-//  disp.clear();
-//  disp.displayInt(temperature);
-//}
