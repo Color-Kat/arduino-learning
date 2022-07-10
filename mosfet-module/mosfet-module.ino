e6e7be8eddffb4ab49747895859b54cbd2c3c2b1
Color-Kat <katik7979@gmail.com>
2022-07-10 16:36:56 +0300

    work with mosfet module. Nothing broke this time;)

--- mosfet-module/mosfet-module.ino
+++ mosfet-module/mosfet-module.ino
@@ -1,28 +1,20 @@
-#define button_pin 13
-#define mosfet_pin 2
+#define mosfet_pin 3
 
-boolean isButtonPressed = 0; // State of button
 boolean mosfet = 0; // State of the mosfet
 
 unsigned long last_click = 0;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(button_pin, INPUT_PULLUP);
     pinMode(mosfet_pin, OUTPUT);
 }
   
 void loop() {
-    boolean button = !digitalRead(button_pin);
 
-    if(button == 1 && isButtonPressed == 0 && millis() - last_click > 50){
+    if(millis() - last_click > 1500){
         last_click = millis();
       
-        isButtonPressed = 1;
         mosfet = !mosfet;
-    }
-    if(button == 0 && isButtonPressed == 1) {
-        isButtonPressed = 0;
     }
 
     Serial.println(mosfet);
